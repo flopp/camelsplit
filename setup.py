58d07c8043e0cae82510be376be3c7a0496e6259
flopp <fpigorsch@gmail.com>
2018-10-08 20:38:56 +0200

    Deploy to PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='camelsplit',
+    version='1.0.0',
+    author='Florian Pigorsch',
+    author_email='mail@florian-pigorsch.de',
+    description='Camel case aware word splitting.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/flopp/camelsplit',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
